30e2674cead5ff354a65fc32e454bf82d9bfba26
Oci <fathorrossi@gmail.com>
2025-08-22 00:31:43 +0700

    add file

---
+++ pertemuan01.py
@@ -0,0 +1,6 @@
+hai1 = " Nama: Muhammad Fathor Rossi"
+hai2 = " NIM:  252410101090"
+hai3 = " Asal: Jember(Mangli)"
+print(f'{hai1} \n {hai2} \n {hai3} ')
+print('=' * 30)
+print(hai2 ,'\n', hai1, '\n', hai3)
